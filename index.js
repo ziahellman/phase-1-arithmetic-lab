1bb093e39a2c7a56be9facb9849c702eb09961f5
Zia Hellman <ziahellman@gmail.com>
2022-05-21 18:12:50 +0300

    commit tree 9815209ecae30474aaa046243a231aee67be5c11

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Write your code he
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+multiply;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10);
+random;
+
+let num3 = 29;
+let num4 = 5;
+let mod = num3 % num4;
+mod;
+
+let max = Math.max(1, 5, 10, 20);
